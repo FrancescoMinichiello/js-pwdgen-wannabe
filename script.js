01923dfbdd27d91f313fbbf77faf728e51549fed
FrancescoMinichiello <fraps2278@gmail.com>
2024-07-31 13:50:42 +0200

    Esercizio finito

---
+++ script.js
@@ -0,0 +1,25 @@
+// ? Chiedere a un utente nome, cognome ed età e stamparlo in pagina
+
+/*
+1- Recupero gli elementi della pagina di mio interesse
+2- Chiedere all'utente il nome
+3- Chiedere all'utente il cognome
+4- Chiedere all'utente il suo colore preferito
+5- Calcolo il nome completo ed il suo colore
+6- stampo il messaggio in pagina
+*/
+
+// # Fase preparatoria
+const userElement = document.getElementById('user')
+console.log('userElement')
+
+
+
+// # Fase di raccolta dati
+const firstName = prompt('Dimmi il tuo nome', 'Francesco')
+const lastName = prompt('Dimmi il tuo cognome', 'Minichiello')
+const favouriteColor = prompt('Dimmi il tuo colore preferito', 'Verde')
+console.log(firstName, lastName, favouriteColor);
+
+// # Fase di produzione output (risultato)
+userElement.innerHTML = firstName + lastName + favouriteColor + '21';
